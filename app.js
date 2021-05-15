54bc0804776459607b8ab97500a1498c1b9501fb
Ray-Shakex <bshakex@gmail.com>
2021-05-15 12:38:51 +0200

    select dom items

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
 // ****** SELECT ITEMS **********
+const alert = document.querySelector('.alert');
+const form = document.querySelector('.grocery-form');
+
+const grocery = document.getElementById('grocery');
+const container = document.querySelector('.grocery-container');
+const list = document.querySelector('.grocery-list');
+
+const submitBtn = document.querySelector('.submit-btn');
+const clearBtn = document.querySelector('.clear-btn');
+
 
 // edit option
 
@@ -9,3 +19,4 @@
 // ****** LOCAL STORAGE **********
 
 // ****** SETUP ITEMS **********
+
