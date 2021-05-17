8d56f9d52f7f9d21ae2d845ffefe0004d2d38db0
Ray-Shakex <bshakex@gmail.com>
2021-05-17 09:53:20 +0200

    if statments setup for addItem()

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # JS-shopping-list
 Vanilla Javascript tutorial project #12 presented by John Smilga
+
+
+### function
+- if(value(is true) && editFlad === false) 
--- app.js
+++ app.js
@@ -23,7 +23,16 @@ addItem = (e) => {
     e.preventDefault();
     const value = grocery.value;
     const id = new Date().getTime().toString();
-    console.log(id);
+    
+    if(value && !editFlag){
+        console.log('add item to the list')
+    }
+    else if(value !=='' && editFlag === true) {
+        console.log('editing');
+    }
+    else{
+        console.log('empty value');
+    }
 }
 
 // submit form
