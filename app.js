fec58c7daf47cc1b879321415718a9cee0654abe
Ray-Shakex <bshakex@gmail.com>
2021-05-18 09:43:37 +0200

    display new items via if statement in addItem()

--- app.js
+++ app.js
@@ -25,13 +25,34 @@ addItem = (e) => {
     const value = grocery.value;
     const id = new Date().getTime().toString();
     
-    if(value && !editFlag){
+    if (value && !editFlag) {
+        const element = document.createElement('article');
+        // add class to new item for display
+        element.classList.add('grocery-item');
+        // add id
+        const attribute = document.createAttribute('data-id');
+        attribute.value = id;
+        element.setAttributeNode(attribute);
+        element.innerHTML = `<p class="title">${value}</p>
+            <div class="btn-container">
+                <button type="button" class="edit-btn">
+                    <i class="fas fa-edit"></i>
+                </button>
+                <button type="button" class="delete-btn">
+                    <i class="fas fa-trash"></i>
+                </button>
+            </div>`
+            //append child
+            list.appendChild(element);
+            //display alert 
+            displayAlert('item added to list', 'success');
+            //show container 
+            container.classList.add('show-container');
+        console.log(element);
         console.log('add item to the list')
-    }
-    else if(value !=='' && editFlag === true) {
+    } else if (value !== '' && editFlag === true) {
         console.log('editing');
-    }
-    else{
+    } else {
         displayAlert('please enter value', 'danger');
     }
 }
@@ -43,10 +64,10 @@ displayAlert = (text, action) => {
     alert.classList.add(`alert-${action}`);
 
     //remove alert
-    setTimeout(()=>{
+    setTimeout(() => {
         alert.textContent = '';
-    alert.classList.remove(`alert-${action}`);
-    }, 1500);
+        alert.classList.remove(`alert-${action}`);
+    }, 2000);
 }
 //end of display alert
 
